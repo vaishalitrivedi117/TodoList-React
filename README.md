09a6d396bcc84e54fee542650b7652c2d94af0f5
vaishalitrivedi117 <58921646+vaishalitrivedi117@users.noreply.github.com>
2020-01-14 14:07:44 +1100

    Initial commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# TodoList-React
-Created with CodeSandbox
---
+++ src/components/pages/About.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function About() {
+  return (
+    <>
+      <h1>About Us</h1>
+      <p>This is about page</p>
+    </>
+  );
+}
+
+export default About;
